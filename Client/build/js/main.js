edbd0519274627921d1a3f79243c755614c8df23
Bruno Hiroki Nagao Anhaia <bruno@anhaiaimoveis.com.br>
2018-12-08 19:57:59 -0200

    datatable

--- Client/build/js/main.js
+++
@@ -1,9 +0,0 @@
-
-
-$('#first-datatable-output table').datatable({
-    pageSize: 5,
-    sort: [true, true, false],
-    filters: [true, false, 'select'],
-    filterText: 'Type to filter... ',
-    pagingDivSelector: "#paging-first-datatable"
-});
