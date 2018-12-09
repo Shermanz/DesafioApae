bc76e65397c0384ea43d17bb08f13f6b3b496bd8
angelinopires <lino.pires@live.com>
2018-12-09 06:49:25 -0200

    final Front

--- Client/src/js/main.js
+++ Client/src/js/main.js
@@ -1,34 +1,102 @@
-validateCpf();
-
-function changeData() {
-    const submit = document.querySelector('.info__btn-pesquisar');
-
-    submit.addEventListener('click', e => {
-        e.preventDefault();
+$(document).ready(function(){
+    $.ajax({
+        url: 'http://172.16.7.17:8000/api/transaction',
+         success: function(data,err) {
+            console.log(data);
+            console.log(err);
+            var somaTotal = 0;
+            data.donation.forEach(e => {
+                somaTotal = somaTotal + e.value;
+                $('.feedback__totalArrecadado').text(`R$ ${somaTotal},00`);
+            });
+            data.donation.forEach((e) => {
+                $("table tbody").append(`
+                    <tr>
+                        <td>${moment((e.created_at)).format("DD/MM/YYYY")}</td>
+                        <td>${e.name}</td>
+                        <td>${e.document}</td>
+                        <td>R$ ${(e.value)},00</td>
+                    </tr>`
+                )
+            });
+        },
+        complete : function(){
+        },
+        beforeSend : function(){
+            $('table tbody').empty();
+        }
     });
-}
+    validateCpf();
+    datePicker();
+    // tableResponsive();
+});
 
 function validateCpf() {
     var options =  {
         onKeyPress: function(cep, e, field, options) {
           var masks = ['000.000.000/000', '00.000.000/0000-00'];
-          var mask = (cep.length>14) ? masks[1] : masks[0];
-          $('#cpf').mask(mask, options);
+          var mask = (cep.length > 14) ? masks[1] : masks[0];
+          $('.input__cpf').mask(mask, options);
     }};
       
-    $('#cpf').mask('000.000.000/00', options);
+    $('.input__cpf').mask('000.000.000/00', options);
 }
 
-$('.info__input').daterangepicker({
-    opens: 'left',
-    locale: {
-        format: 'YYYY/MM/DD'
-    },
-    minDate: '2018/01/01',  
-    startDate: '2018/01/01'
-});
+function datePicker() {
+    $('.input__data').daterangepicker({
+        opens: 'left',
+        locale: {
+            format: 'YYYY/MM/DD'
+        },
+        minDate: '2018/01/01',  
+        startDate: '2018/01/01'
+    });
+    
+    $('.input__data').on('apply.daterangepicker', function(ev, picker) {
+        console.log(picker.startDate.format('YYYY-MM-DD'));
+        console.log(picker.endDate.format('YYYY-MM-DD'));
+      });
+}
 
-$('.info__input').on('apply.daterangepicker', function(ev, picker) {
-    console.log(picker.startDate.format('YYYY-MM-DD'));
-    console.log(picker.endDate.format('YYYY-MM-DD'));
-  });
+function tableResponsive() {
+    $('table').basictable({
+        breakpoint: 768,
+        forceResponsive: true
+    });
+}
+
+$('.btn-pesquisar').on('click', function(){
+    console.log($('.input__cpf').val());
+
+    $.ajax({
+        url: 'http://172.16.7.17:8000/api/transaction/user',
+        body: '{ document: asdasdas}',
+        method: 'POST',
+         success: function(data,err) {
+            console.log(data);
+            console.log(err);
+            var somaTotal = 0;
+            data.donation.forEach(e => {
+                somaTotal = somaTotal + e.value;
+                $('.feedback__totalArrecadado').text(`R$ ${somaTotal},00`);
+            });
+            data.donation.forEach((e) => {
+                $("table tbody").append(`
+                    <tr>
+                        <td>${moment((e.created_at)).format("DD/MM/YYYY")}</td>
+                        <td>${e.name}</td>
+                        <td>${e.document}</td>
+                        <td>R$ ${(e.value)},00</td>
+                    </tr>`
+                )
+            });
+        },
+        complete : function(){
+        },
+        beforeSend : function(){
+            $('table tbody').empty();
+        }
+    });
+
+
+});
